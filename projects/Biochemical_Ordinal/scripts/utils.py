951b30ea8371676e52f32da678b464952021d6b1
Sameer D'Costa <sameerdcosta@gmail.com>
2021-10-14 09:57:06 -0500

    Adding beneficial mutations

--- projects/Biochemical_Ordinal/scripts/utils.py
+++ projects/Biochemical_Ordinal/scripts/utils.py
@@ -1,3 +1,7 @@
 def hamming_dist(s1, s2):
     assert(len(s1) == len(s2))
     return sum(1 for (a, b) in zip(s1, s2) if a != b)
+
+def mut_as_string(mut, wt, offset=0, mut_sep=","):
+    return mut_sep.join([f"{w}{i+offset}{m}" for
+                i, (m,w) in enumerate(zip(mut, wt)) if m != w])
