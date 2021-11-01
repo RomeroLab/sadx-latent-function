926fbab232ba1036d212d0c60360cc1ce993b58d
Sameer D'Costa <sameerdcosta@gmail.com>
2021-11-01 12:36:46 -0500

    Disabling output of structures and only saving score files

--- projects/Biochemical_Ordinal/rosetta/ligand_dock.sh
+++ projects/Biochemical_Ordinal/rosetta/ligand_dock.sh
@@ -70,7 +70,7 @@ ROSETTA3_DB=${DATABASE_PATH} ${ROSETTA_SCRIPTS_BIN} \
 
 # This file should be returned
 # We should name it something appropriate
-mv Models/score.sc ..
+mv Models/scores.sc ..
 
 
 
