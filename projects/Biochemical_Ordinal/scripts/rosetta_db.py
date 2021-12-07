251fca00edb5eda5d978ed0eefa7ec5b2f766709
Sameer D'Costa <sameerdcosta@gmail.com>
2021-12-07 09:51:27 -0600

    Post-process saves train and test datasets

--- projects/Biochemical_Ordinal/scripts/rosetta_db.py
+++ projects/Biochemical_Ordinal/scripts/rosetta_db.py
@@ -1,15 +1,14 @@
 import pathlib
-
 import tarfile
-import pathlib
 
 import sqlite3
 import yaml
 import tqdm
 
+import numpy as np
 import pandas as pd
 
-SQLITE3_DB_LOC="../data/scratch/rosetta" 
+ROSETTA_SCRATCH_DIR="../data/scratch/rosetta"
 
 def get_sqlite_dbcon(parent="WT"):
     """ parent can be WT or 2D """
@@ -17,7 +16,15 @@ def get_sqlite_dbcon(parent="WT"):
 
 def get_sqlite_db(parent):
     """ parent can be WT or 2D """
-    return SQLITE3_DB_LOC + "/" + parent + ".db"
+    return pathlib.Path(ROSETTA_SCRATCH_DIR) / (parent + ".db")
+
+def get_splits_dir(parent, mkdir=True):
+    if parent not in ["2D", "WT"]:
+        raise ValueError("parent must be 2D or WT. Got : " + parent)
+    retdir = pathlib.Path(ROSETTA_SCRATCH_DIR) / (parent + "_splits")
+    if mkdir:
+        retdir.mkdir(parents=False, exist_ok=True)
+    return retdir
 
 def read_rosetta_score_file(fh):
     ret = pd.read_csv(fh, delim_whitespace=True, skiprows=1)
@@ -46,6 +53,52 @@ def load_chtc_results_to_sqlite(tar_gz_filelist, db_con):
         docked_score_df.to_sql(name="docked_scores", con=db_con, 
                  if_exists='append', index=False,
                  dtype={"variant":'text', "description":'text'})
+
+def get_energy_scores_from_sqlite(db_con, remove_description=True):
+    """ Get an inner join of relaxed scores and docking scores
+
+        remove_description : remove columns r_description and d_description
+            which specify which model was selected by Rosetta out of the 
+            hundreds of models that it built (nstructs). All remaining scores
+            should be floats. 
+
+    """
+    df = pd.read_sql("select r.*, d.* from relaxed_scores as r "
+                     "inner join docked_scores as d on d.variant = r.variant;"
+                     , db_con)
+
+    # We have duplicated column names because they have similar names in the
+    # two columns now relabel the columns so that the first columns are
+    # prepended with r_ for relaxation and the second set of columns are
+    # prepended with d_ for docking
+
+    # description should occur twice in the database columns. Find out where
+    desciption_idxs = np.where(df.columns == "description")[0]
+    assert(len(desciption_idxs) == 2)
+    table_split = desciption_idxs[0] + 1
+
+    df.columns = ["r_" + c for c in df.columns[:table_split]] \
+                 + ["d_" + c for c in df.columns[table_split:-1]] \
+                 + [df.columns[-1]] # this is "variant", the key for joining 
+    
+    columns_to_drop = ["d_variant"]
+
+    if remove_description: # which structure was selected. 
+        columns_to_drop += ["r_description","d_description"]
+
+    df.drop(columns=columns_to_drop, inplace=True)
+
+    return df
+
+def get_energy_scores(parent):
+    db_con = get_sqlite_dbcon(parent=parent)
+    df = get_energy_scores_from_sqlite(db_con, remove_description=True)
+    db_con.close()
+    return df
+
+
+
+
 
 
 if __name__ == "__main__":
