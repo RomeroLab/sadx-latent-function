48f0222e71c8d279fdc7567c6fd25f0edb0860a9
Sameer D'Costa <sameerdcosta@gmail.com>
2023-04-13 12:41:45 -0500

    changes to alphafold environment

--- projects/protein_utils/chtc/alphafold/create_alphafold_conda.sh
+++ projects/protein_utils/chtc/alphafold/create_alphafold_conda.sh
@@ -1,22 +1,41 @@
 
 ```shell
-conda create -n alphafold python=3.8
+conda create -n alphafold python=3.9
 conda activate alphafold
 
-wget https://raw.githubusercontent.com/deepmind/alphafold/main/requirements.txt
-pip3 install -r requirements.txt
-rm requirements.txt
+pip3 install py3dmol
+conda install -qy conda==4.13.0
+# extra stuff for interactively using alphafold
+conda install -qy -c conda-forge python=3.9 openmm=7.5.1 pdbfixer notebook ipython
+#ipython kernel install --name "alphafold-conda" --user
+#conda install -qy -c conda-forge python=3.9 openmm=7.5.1 pdbfixer 
 
-pip3 install notebook ipython seaborn
-ipython kernel install --name "alphafold-conda" --user
+cd ~
+git clone --branch main https://github.com/deepmind/alphafold alphafold
+pip3 install -r ~/alphafold/requirements.txt
+
+pip3 install --no-dependencies ./alphafold
+#pip3 install --upgrade pyopenssl
+
+# patch openmm
+(cd ~/miniconda3/envs/alphafold/lib/python3.9/site-packages; patch -p0 < ~/alphafold/docker/openmm.patch)
+# stereo_chemical properties
+(cd ~/miniconda3/envs/alphafold/lib/python3.9/site-packages/alphafold/common; wget https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt)
 
 conda deactivate
 
 # install conda-pack if we dont already have it
 # conda install -c conda-forge conda-pack
 
-conda pack -n alphafold
-mv alphafold.tar.gz 
-mv alphafold.tar.gz /squid/dcosta2/conda_alphafold_py38.tar.gz
+conda pack --ignore-missing-files -n alphafold
+split -b 900M alphafold.tar.gz alphafold_conda_tar_gz_
+mv alphafold_conda_tar_gz_a* /squid/dcosta2/py39
+
+# parameter are around 3.8GB
+wget https://storage.googleapis.com/alphafold/alphafold_params_colab_2022-12-06.tar
+split -b 950M alphafold_params_colab_2022-12-06.tar  /squid/dcosta2/alphafold_params/alphafold_params_colab_tar_
+mv alphafold_params_colab_tar_a* /squid/dcosta2/alphafold_params
+
+rm alphafold.tar.gz alphafold_params_colab_2022-12-06.tar
 
 ```
