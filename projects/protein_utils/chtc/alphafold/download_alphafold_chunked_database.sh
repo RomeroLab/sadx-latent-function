8303a0cddce730f477e45172be03e9d0bf0e8c2f
Sameer Dcosta <sameerdcosta@Sameers-MacBook-Pro.local>
2023-04-17 12:25:39 -0500

    adding uniprot and gzip

--- projects/protein_utils/chtc/alphafold/download_alphafold_chunked_database.sh
+++ projects/protein_utils/chtc/alphafold/download_alphafold_chunked_database.sh
@@ -5,15 +5,24 @@ DB_ROOT_PATH=https://storage.googleapis.com/alphafold-colab/latest
 
 for i in {1..62}
 do
-  echo wget ${DB_ROOT_PATH}/uniref90_2022_01.fasta.${i}
+  wget ${DB_ROOT_PATH}/uniref90_2022_01.fasta.${i}
+  gzip uniref90_2022_01.fasta.${i} &
 done
 for i in {1..17}
 do
-  echo wget ${DB_ROOT_PATH}/bfd-first_non_consensus_sequences.fasta.${i}
+  wget ${DB_ROOT_PATH}/bfd-first_non_consensus_sequences.fasta.${i}
+  gzip bfd-first_non_consensus_sequences.fasta.${i} &
 done
 for i in {1..120}
 do
-  echo wget ${DB_ROOT_PATH}/mgy_clusters_2022_05.fasta.${i}
+  wget ${DB_ROOT_PATH}/mgy_clusters_2022_05.fasta.${i}
+  gzip mgy_clusters_2022_05.fasta.${i} &
 done
+for i in {1..101}
+do
+  wget ${DB_ROOT_PATH}/uniref90_2022_01.fasta.${i}
+  gzip uniref90_2022_01.fasta.${i} &
+done
+
 
 
