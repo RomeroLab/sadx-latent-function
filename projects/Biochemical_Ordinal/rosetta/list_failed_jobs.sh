2cffc332038ee06f586aebc28a654fcc4fbd4899
Sameer D'Costa <sameerdcosta@gmail.com>
2023-07-25 10:11:52 -0500

    Looking in two directories for results

--- projects/Biochemical_Ordinal/rosetta/list_failed_jobs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# look for jobs that don't have any output. (take 4.0K of space)
-# jobs that work take up around 1M of space. 
-# FIXME: Use filesize
-
-ls -sk SadA_*.tar.gz | awk '{if ($1 < 10) print $2 }' | cut -d_ -f2
--- projects/Biochemical_Ordinal/rosetta/list_jobs_to_redo.sh
+++ projects/Biochemical_Ordinal/rosetta/list_jobs_to_redo.sh
@@ -7,5 +7,5 @@
 # Then we print out which files are unique to the original variant list
 
 comm <(sort SadA_NSLeu_Corrected_3701_best_structure_0044.single_mutants.txt) \
-     <(ls -sk SadA_*.tar.gz | awk '{if ($1 > 1000) print $2 }' | cut -d_ -f2 | sort) \
+     <(ls -sk SadA_*.tar.gz results/SadA_*.tar.gz | awk '{if ($1 > 1000) print $2 }' | cut -d_ -f2 | sort) \
      -3  
