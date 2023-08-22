9a8808a54d1451b9535fbe9b1bcf63ba791650a1
Sameer D'Costa <sameerdcosta@gmail.com>
2023-08-22 13:59:46 -0500

    changing followup scripts to discriminate between output and the logs output
    eg.  SadA_Y96C_rosetta.tar.gz vs SadA_Y96D_logs_rosetta.tar.gz

--- projects/Biochemical_Ordinal/rosetta/list_jobs_to_redo_all.sh
+++ projects/Biochemical_Ordinal/rosetta/list_jobs_to_redo_all.sh
@@ -6,6 +6,11 @@
 # We find out which ones are greater than 1M and make a sorted list
 # Then we print out which files are unique to the original variant list
 
+shopt -s extglob
+
 comm <(sort SadA_NSLeu_Corrected_3701_best_structure_0044.single_mutants.txt) \
-     <(ls -sk SadA_*.tar.gz results/SadA_*.tar.gz | awk '{if ($1 > 1000) print $2 }' | cut -d_ -f2 | sort) \
+     <(ls -sk SadA_+([[:alnum:]])_rosetta.tar.gz \
+            results/SadA_+([[:alnum:]])_rosetta.tar.gz \
+        | awk '{if ($1 > 1000) print $2 }' \
+        | cut -d_ -f2 | sort) \
      -3  
--- projects/Biochemical_Ordinal/rosetta/list_jobs_to_redo_small.sh
+++ projects/Biochemical_Ordinal/rosetta/list_jobs_to_redo_small.sh
@@ -5,5 +5,11 @@
 # ls -sk is filesize in kilobytes
 # We find out which ones are smaller than 1M and make a sorted list
 
-ls -sk SadA_*.tar.gz results/SadA_*.tar.gz | awk '{if ($1 < 1000) print $2 }' | cut -d_ -f2 | sort
+shopt -s extglob
+
+(ls -sk SadA_+([[:alnum:]])_rosetta.tar.gz \
+        results/SadA_+([[:alnum:]])_rosetta.tar.gz 2> /dev/null) \
+         | awk '{if ($1 < 1000) print $2 }' \
+         | cut -d_ -f2 \
+         | sort
 
