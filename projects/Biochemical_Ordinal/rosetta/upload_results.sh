2feaf10618c67521f8c0b1a7321d3071f7d9e8d5
Sameer D'Costa <sameerdcosta@gmail.com>
2023-08-25 14:20:59 -0500

    script to upload results to rdrive

---
+++ projects/Biochemical_Ordinal/rosetta/upload_results.sh
@@ -0,0 +1,10 @@
+upload_tag="andres_temp_5_wholeseparaterelax_rep_1"
+smbclient -k //research.drive.wisc.edu/promero2 <<SMBCLIENTCOMMANDS
+prompt
+cd General/Sameer/SadA_azidation/rosetta/
+mkdir $upload_tag
+cd $upload_tag
+lcd results
+mput *.tar.gz
+exit
+SMBCLIENTCOMMANDS
