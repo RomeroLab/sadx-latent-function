8440f18550655cf15d9998b9641d129be7a88076
Sameer D'Costa <sameerdcosta@gmail.com>
2023-09-06 07:35:17 -0500

    Generating variants based on chain

--- projects/Biochemical_Ordinal/rosetta/generate_variants.py
+++ projects/Biochemical_Ordinal/rosetta/generate_variants.py
@@ -6,12 +6,14 @@ from Bio import BiopythonParserWarning
 
 protein_letters = Bio.Data.IUPACData.protein_letters
 
-def wt_seq_from_pdb(filename):
+def wt_seq_from_pdb(filename, chain="A"):
     ret = None
     with warnings.catch_warnings():
         warnings.simplefilter('ignore', BiopythonParserWarning)
-        ret = Bio.SeqIO.read(filename, "pdb-atom")
-        ret = str(ret.seq)
+        for record in Bio.SeqIO.parse(filename, "pdb-atom"):
+            if record.annotations["chain"] == chain:
+                ret = str(record.seq)
+                break
     return ret
 
 def generate_single_mutants(wt_seq):
@@ -30,9 +32,13 @@ if __name__ == "__main__":
                     type=argparse.FileType('r'),
                     required=True,
                     help="PARENT to start with")
+    parser.add_argument("-c", "--chain",
+                    type=str,
+                    default="A",
+                    help="Which chain to use")
     args = parser.parse_args()
 
-    wt_seq = wt_seq_from_pdb(args.pdb)
+    wt_seq = wt_seq_from_pdb(args.pdb, chain=args.chain)
     # let's include the WT sequence as the first variant
     # This is useful to compare other energies to
     print(f"X1{wt_seq[0]}")
